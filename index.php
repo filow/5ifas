e2a14f94478c254075d9cd5000e1b157b3b28cb2
filow <lirenhai427@gmail.com>
2013-09-28 13:13:06 +0800

    恢复首页

--- index.php
+++ index.php
@@ -1,23 +1,9 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>艾星网络</title>
-</head>
-<body>
-    <a href="admin.php">前台暂时关闭,点击转到后台首页</a>
-</body>
-</html>
-
-
-
-
-<?php
-// define('SITE_ADDR',substr($_SERVER['SCRIPT_NAME'],0,strlen($_SERVER['SCRIPT_NAME'])-10));
-// define('THINK_PATH','./ThinkPHP/'); //thinkphp框架的地址
-// define('APP_NAME','Home');          //预设app的名称  一般是home
-// define('APP_PATH','./Home/');       //预设app的地址
-// define('APP_DEBUG',true);            //调试模式
-// //define('SAE_RUNTIME',true);       
-// require './ThinkPHP/Extend/Engine/Sae.php';  //sae引擎
+<?php
+define('SITE_ADDR',substr($_SERVER['SCRIPT_NAME'],0,strlen($_SERVER['SCRIPT_NAME'])-10));
+define('THINK_PATH','./ThinkPHP/'); //thinkphp框架的地址
+define('APP_NAME','Home');          //预设app的名称  一般是home
+define('APP_PATH','./Home/');       //预设app的地址
+define('APP_DEBUG',true);            //调试模式
+//define('SAE_RUNTIME',true);       
+require './ThinkPHP/Extend/Engine/Sae.php';  //sae引擎
 ?>
