dfab3daee78600d516a8f315efd5dd2e931eeaf4
filow <lirenhai427@gmail.com>
2013-09-28 19:16:44 +0800

    修复在sae上出错的问题

--- index.php
+++ index.php
@@ -5,5 +5,6 @@ define('APP_NAME','Home');          //预设app的名称  一般是home
 define('APP_PATH','./Home/');       //预设app的地址
 define('APP_DEBUG',true);            //调试模式
 //define('SAE_RUNTIME',true);       
+define('ENGINE_PATH', THINK_PATH . '/Extend/Engine/');
 require './ThinkPHP/Extend/Engine/Sae.php';  //sae引擎
 ?>
